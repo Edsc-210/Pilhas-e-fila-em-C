753c737e5fd60a62fd3887a0e1841ffe0e240d66
Edsc-210 <diogonino202@gmail.com>
2025-11-26 00:15:34 +0000

    nivel 2, adicionando pilhas no codigo

--- filasEPilhas.c
+++ filasEPilhas.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 #define MAX 5
+#define MAX_PILHA 5
 
 typedef struct {
     char nome;
@@ -103,11 +104,68 @@ void exibirFila(Fila f) {
     printf("\n");
 }
 
+typedef struct {
+    Peca itens[MAX_PILHA];
+    int topo;
+} Pilha;
+
+void inicializarPilha(Pilha *p) {
+    p->topo = -1;
+}
+
+int pilhaVazia(Pilha *p) {
+    return p->topo == -1;
+}
+
+int pilhaCheia(Pilha *p) {
+    return p->topo == MAX_PILHA - 1;
+}
+
+void push(Pilha *p, Peca nova) {
+    if (pilhaCheia(p)) {
+        printf("Pilha cheia. Não é possível inserir.\n");
+        return;
+    }
+ 
+    p->topo++;
+    p->itens[p->topo] = nova;
+}
+
+void pop(Pilha *p, Peca *removida) {
+    if (pilhaVazia(p)) {
+        printf("Pilha vazia. Não é possível remover.\n");
+        return;
+    }
+
+    *removida = p->itens[p->topo];
+    p->topo--;
+}
+
+void peek(Pilha *p, Peca *visualizada) {
+    if (pilhaVazia(p)) {
+        printf("Pilha vazia. Nada para espiar.\n");
+        return;
+    }
+
+    *visualizada = p->itens[p->topo];
+}
+
+void mostrarPilha(Pilha *p) {
+    printf("Pilha (topo -> base):\n");
+    for (int i = p->topo; i >= 0; i--) {
+        printf("[%c, %d]\n", p->itens[i].nome, p->itens[i].id);
+    }
+    printf("\n");
+}
+
 int main() {
     srand(time(NULL));
 
     Fila f;
     inicializarFila(&f);  // Inicializa a fila
+
+    Pilha p;
+    inicializarPilha(&p);
 
     // Vamos encher a fila antes de começar o jogo
     printf("--- Inicializando o sistema ---\n");
@@ -121,9 +179,11 @@ int main() {
     do {
         printf("\n=== TETRIS STACK (Fila: %d/%d) ===\n", f.total, MAX);
         exibirFila(f); //chama função de exibir aqui
-        
+        mostrarPilha(&p); //Mostra a pilha em baixo da fila
+
         printf("1. Jogar peca (Remover)\n");
-        printf("2. Inserir nova peca\n");
+        printf("2. Reservar peça\n");
+        printf("3. Usar peça reservada\n");
         printf("0. Sair\n");
         printf("Escolha: ");
         scanf("%d", &opcao);
@@ -133,9 +193,33 @@ int main() {
                 // Passa o endereço da fila (&f) e o endereço da peca temporaria (&pecaRemovida)
                 jogar(&f, &pecaRemovida); 
                 printf(">>> Voce jogou a peca: [%c %d] <<<\n", pecaRemovida.nome, pecaRemovida.id);
+                inserir(&f);
                 break;
             case 2:
-                inserir(&f);
+                // Verifica se a pilha cabe mais
+                if (!pilhaCheia(&p)) {
+                // 1. Tira da fila 
+                jogar(&f, &pecaRemovida); 
+        
+                // 2. Coloca na pilha
+                push(&p, pecaRemovida); 
+        
+                printf(">>> Voce reservou a peca: [%c %d] <<<\n", pecaRemovida.nome, pecaRemovida.id);
+        
+                // 3. Importante: Repor a fila!
+                inserir(&f); 
+                } else {
+                    printf("Pilha cheia!\n");
+                }
+            break;
+            case 3 :
+                if (!pilhaVazia(&p)) {
+                    pop(&p, &pecaRemovida);
+                    printf(">>> Você usou a peça reservada: [%c %d] <<<\n", pecaRemovida.nome, pecaRemovida.id);      
+                }
+                else {
+                    printf("Não ha peças na reserva!\n");
+                }
                 break;
             case 0:
                 printf("Saindo...\n");
